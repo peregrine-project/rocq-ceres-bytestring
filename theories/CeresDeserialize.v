bc17ba0b6fee1f075f087f75858af9f750290fdb
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-02-26 21:35:04 +0100

    Add extra con definitions

--- theories/CeresDeserialize.v
+++ theories/CeresDeserialize.v
@@ -216,6 +216,199 @@ Definition con5 {A B C D E R} (f : A -> B -> C -> D -> E -> R)
     fields (pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
     ret (f a b c d e)).
 
+Definition con6
+  {A B C D E F R}
+  (f : A -> B -> C -> D -> E -> F -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexpList R :=
+  fun pa pb pc pd pe pf =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' =>
+        Deser.ret (f a b c d e f')).
+
+Definition con7
+  {A B C D E F G R}
+  (f : A -> B -> C -> D -> E -> F -> G -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g =>
+        Deser.ret (f a b c d e f' g)).
+
+Definition con8
+  {A B C D E F G H R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h =>
+        Deser.ret (f a b c d e f' g h)).
+
+Definition con9
+  {A B C D E F G H I R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i =>
+        Deser.ret (f a b c d e f' g h i)).
+
+Definition con10
+  {A B C D E F G H I J R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+        Deser.ret (f a b c d e f' g h i j)).
+
+Definition con11
+  {A B C D E F G H I J K R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k =>
+        Deser.ret (f a b c d e f' g h i j k)).
+
+Definition con12
+  {A B C D E F G H I J K L R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexp L
+      -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk pl =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k => pl >>= fun l =>
+        Deser.ret (f a b c d e f' g h i j k l)).
+
+Definition con13
+  {A B C D E F G H I J K L M R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexp L
+      -> FromSexp M -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk pl pm =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k => pl >>= fun l => pm >>= fun m =>
+        Deser.ret (f a b c d e f' g h i j k l m)).
+
+Definition con14
+  {A B C D E F G H I J K L M N R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexp L
+      -> FromSexp M -> FromSexp N -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk pl pm pn =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k => pl >>= fun l => pm >>= fun m => pn >>= fun n =>
+        Deser.ret (f a b c d e f' g h i j k l m n)).
+
+Definition con15
+  {A B C D E F G H I J K L M N O R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexp L
+      -> FromSexp M -> FromSexp N -> FromSexp O -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk pl pm pn po =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k => pl >>= fun l => pm >>= fun m => pn >>= fun n => po >>= fun o =>
+        Deser.ret (f a b c d e f' g h i j k l m n o)).
+
+Definition con16
+  {A B C D E F G H I J K L M N O P R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexp L
+      -> FromSexp M -> FromSexp N -> FromSexp O -> FromSexp P -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk pl pm pn po pp =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k => pl >>= fun l => pm >>= fun m => pn >>= fun n => po >>= fun o =>
+      pp >>= fun p =>
+        Deser.ret (f a b c d e f' g h i j k l m n o p)).
+
+Definition con17
+  {A B C D E F G H I J K L M N O P Q R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> Q -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexp L
+      -> FromSexp M -> FromSexp N -> FromSexp O -> FromSexp P -> FromSexp Q -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk pl pm pn po pp pq =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k => pl >>= fun l => pm >>= fun m => pn >>= fun n => po >>= fun o =>
+      pp >>= fun p => pq >>= fun q =>
+        Deser.ret (f a b c d e f' g h i j k l m n o p q)).
+
+Definition con18
+  {A B C D E F G H I J K L M N O P Q S R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> Q -> S -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+    -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexp L
+    -> FromSexp M -> FromSexp N -> FromSexp O -> FromSexp P -> FromSexp Q -> FromSexp S
+    -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk pl pm pn po pp pq ps =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k => pl >>= fun l => pm >>= fun m => pn >>= fun n => po >>= fun o =>
+      pp >>= fun p => pq >>= fun q => ps >>= fun s =>
+        Deser.ret (f a b c d e f' g h i j k l m n o p q s)).
+
+Definition con19
+  {A B C D E F G H I J K L M N O P Q S T R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> Q -> S -> T -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexp L
+      -> FromSexp M -> FromSexp N -> FromSexp O -> FromSexp P -> FromSexp Q -> FromSexp S
+      -> FromSexp T -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk pl pm pn po pp pq ps pt =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k => pl >>= fun l => pm >>= fun m => pn >>= fun n => po >>= fun o =>
+      pp >>= fun p => pq >>= fun q => ps >>= fun s => pt >>= fun t =>
+        Deser.ret (f a b c d e f' g h i j k l m n o p q s t)).
+
+Definition con20
+  {A B C D E F G H I J K L M N O P Q S T U R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> Q -> S -> T -> U -> R)
+  : FromSexp A -> FromSexp B -> FromSexp C -> FromSexp D -> FromSexp E -> FromSexp F
+      -> FromSexp G -> FromSexp H -> FromSexp I -> FromSexp J -> FromSexp K -> FromSexp L
+      -> FromSexp M -> FromSexp N -> FromSexp O -> FromSexp P -> FromSexp Q -> FromSexp S
+      -> FromSexp T -> FromSexp U -> FromSexpList R :=
+  fun pa pb pc pd pe pf pg ph pi pj pk pl pm pn po pp pq ps pt pu =>
+    Deser.fields (
+      pa >>= fun a => pb >>= fun b => pc >>= fun c => pd >>= fun d => pe >>= fun e =>
+      pf >>= fun f' => pg >>= fun g => ph >>= fun h => pi >>= fun i => pj >>= fun j =>
+      pk >>= fun k => pl >>= fun l => pm >>= fun m => pn >>= fun n => po >>= fun o =>
+      pp >>= fun p => pq >>= fun q => ps >>= fun s => pt >>= fun t => pu >>= fun u =>
+        Deser.ret (f a b c d e f' g h i j k l m n o p q s t u)).
+
+
 Definition con1_ {A R} (f : A -> R) `{Deserialize A} : FromSexpList R :=
   con1 f _from_sexp.
 Definition con2_ {A B R} (f : A -> B -> R) `{Deserialize A} `{Deserialize B} : FromSexpList R :=
@@ -230,6 +423,160 @@ Definition con5_ {A B C D E R} (f : A -> B -> C -> D -> E -> R)
     `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
   : FromSexpList R :=
   con5 f  _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp.
+
+Definition con6_
+  {A B C D E F R}
+  (f : A -> B -> C -> D -> E -> F -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F}
+  : FromSexpList R :=
+  con6 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp.
+
+Definition con7_
+  {A B C D E F G R}
+  (f : A -> B -> C -> D -> E -> F -> G -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G}
+  : FromSexpList R :=
+  con7 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp.
+
+Definition con8_
+  {A B C D E F G H R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H}
+  : FromSexpList R :=
+  con8 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp.
+
+Definition con9_
+  {A B C D E F G H I R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I}
+  : FromSexpList R :=
+  con9 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp.
+
+Definition con10_
+  {A B C D E F G H I J R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  : FromSexpList R :=
+  con10 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp.
+
+Definition con11_
+  {A B C D E F G H I J K R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K}
+  : FromSexpList R :=
+  con11 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp.
+
+Definition con12_
+  {A B C D E F G H I J K L R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K} `{Deserialize L}
+  : FromSexpList R :=
+  con12 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp.
+
+Definition con13_
+  {A B C D E F G H I J K L M R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K} `{Deserialize L} `{Deserialize M}
+  : FromSexpList R :=
+  con13 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp.
+
+Definition con14_
+  {A B C D E F G H I J K L M N R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K} `{Deserialize L} `{Deserialize M} `{Deserialize N}
+  : FromSexpList R :=
+  con14 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp.
+
+Definition con15_
+  {A B C D E F G H I J K L M N O R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K} `{Deserialize L} `{Deserialize M} `{Deserialize N} `{Deserialize O}
+  : FromSexpList R :=
+  con15 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp.
+
+Definition con16_
+  {A B C D E F G H I J K L M N O P R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K} `{Deserialize L} `{Deserialize M} `{Deserialize N} `{Deserialize O}
+  `{Deserialize P}
+  : FromSexpList R :=
+  con16 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp.
+
+Definition con17_
+  {A B C D E F G H I J K L M N O P Q R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> Q -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K} `{Deserialize L} `{Deserialize M} `{Deserialize N} `{Deserialize O}
+  `{Deserialize P} `{Deserialize Q}
+  : FromSexpList R :=
+  con17 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp.
+
+Definition con18_
+  {A B C D E F G H I J K L M N O P Q S R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> Q -> S -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K} `{Deserialize L} `{Deserialize M} `{Deserialize N} `{Deserialize O}
+  `{Deserialize P} `{Deserialize Q} `{Deserialize S}
+  : FromSexpList R :=
+  con18 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp.
+
+Definition con19_
+  {A B C D E F G H I J K L M N O P Q S T R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> Q -> S -> T -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K} `{Deserialize L} `{Deserialize M} `{Deserialize N} `{Deserialize O}
+  `{Deserialize P} `{Deserialize Q} `{Deserialize S} `{Deserialize T}
+  : FromSexpList R :=
+  con19 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp.
+
+Definition con20_
+  {A B C D E F G H I J K L M N O P Q S T U R}
+  (f : A -> B -> C -> D -> E -> F -> G -> H -> I -> J -> K -> L -> M -> N -> O -> P -> Q -> S -> T -> U -> R)
+  `{Deserialize A} `{Deserialize B} `{Deserialize C} `{Deserialize D} `{Deserialize E}
+  `{Deserialize F} `{Deserialize G} `{Deserialize H} `{Deserialize I} `{Deserialize J}
+  `{Deserialize K} `{Deserialize L} `{Deserialize M} `{Deserialize N} `{Deserialize O}
+  `{Deserialize P} `{Deserialize Q} `{Deserialize S} `{Deserialize T} `{Deserialize U}
+  : FromSexpList R :=
+  con20 f _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp
+    _from_sexp _from_sexp _from_sexp _from_sexp _from_sexp.
+
 
 Class DeserFromSexpList (A R : Type) (n m : nat) :=
   _from_sexp_list : A -> FromSexpListN n m R.
